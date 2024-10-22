51db74ffee0ed86bc46115e9d767b412471f7c48
goharvard77 <goharvard77@gmail.com>
2024-10-22 15:39:00 +0300

    fix netlify

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -4,12 +4,24 @@ exports.handler = async function (event) {
   const apiUrl = `http://api.calmplete.net${event.path.replace('/.netlify/functions/proxy', '')}`;
 
   try {
-    const response = await axios.get(apiUrl);
+    const token = process.env.VITE_API_TOKEN;
+
+    const response = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
       body: JSON.stringify(response.data),
     };
   } catch (error) {
+    console.error('Error fetching data from API:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Error fetching data from API' }),
